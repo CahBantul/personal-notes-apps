fbeacabb5deed3f7fcf34223803e6683212d5647
fardan <fardan.nozami@gmail.com>
2022-06-17 09:58:26 +0700

    add component NoteItemAction.js

---
+++ src/components/NoteItemAction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NoteItemArchiveButton from './NoteItemArchiveButton';
+import NoteItemDeleteButton from './NoteItemDeleteButton';
+
+function NoteItemAction({ id, onDelete, onArchive }) {
+  return (
+    <div className="note-item__action">
+      <NoteItemDeleteButton id={id} onDelete={onDelete} />
+      <NoteItemArchiveButton id={id} onArchive={onArchive} />
+    </div>
+  );
+}
+
+export default NoteItemAction;
