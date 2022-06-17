9e54ce7502cb0c0208902357d253011f2bb17a58
fardan <fardan.nozami@gmail.com>
2022-06-17 09:54:22 +0700

    NoteItemArchivveButton

---
+++ src/components/NoteItemArchiveButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NoteItemArchiveButton({ id, onArchive }) {
+  return <button className="note-item__archive-buttton">Arsipkan</button>;
+}
+
+export default NoteItemArchiveButton;
