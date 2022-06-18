348bad15272337c6b0f94308c88d454f1b8822b4
fardan <fardan.nozami@gmail.com>
2022-06-18 22:09:46 +0700

    pengkondisian noteList kosong done

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -2,9 +2,12 @@ import React from 'react';
 import NoteItem from './NoteItem';
 
 function NoteList({ notes, onDelete, onArchive, itemTitle, buttonTitle }) {
-  return (
-    <>
-      <h2>{itemTitle}</h2>
+  let noteList;
+
+  if (notes.length === 0) {
+    noteList = <p className="notes-list__empty-message">Tidak ada catatan</p>;
+  } else {
+    noteList = (
       <div className="notes-list">
         {notes.map((note) => (
           <NoteItem
@@ -17,6 +20,12 @@ function NoteList({ notes, onDelete, onArchive, itemTitle, buttonTitle }) {
           />
         ))}
       </div>
+    );
+  }
+  return (
+    <>
+      <h2>{itemTitle}</h2>
+      {noteList}
     </>
   );
 }
